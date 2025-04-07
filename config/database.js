ef8bc3a70d9c8fd69d6d234af48fab51ba7f0a43
RozovStanislav <116573407+RozovStanislav@users.noreply.github.com>
2025-04-07 10:41:43 +0300

    database.js

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
+    user: 'postgres.lbywewuxdspuufppebny',
+    host: 'aws-0-us-east-1.pooler.supabase.com',
     database: 'postgres',
     password: '11111',
-    port: 5432,
+    pool_mode 'transaction',
+    port: 6543,
 });
 
 client.connect();
