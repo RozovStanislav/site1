b3ed6a6b77df61360d32c4c96c172523b32e32e3
RozovStanislav <116573407+RozovStanislav@users.noreply.github.com>
2025-04-07 11:03:14 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,11 +4,16 @@ const client = new Client({
     user: 'postgres.lbywewuxdspuufppebny',
     host: 'aws-0-us-east-1.pooler.supabase.com',
     database: 'postgres',
-    password: '11111',
-    pool_mode 'transaction',
+    password: 'lIBUUzubN5GPF9da',
     port: 6543,
+    ssl: {
+        rejectUnauthorized: true
+    },
+    connectionString: 'postgres://postgres.lbywewuxdspuufppebny:lIBUUzubN5GPF9da@aws-0-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x'
 });
 
-client.connect();
+client.connect()
+    .then(() => console.log('Connected to PostgreSQL database'))
+    .catch(err => console.error('Connection error', err.stack));
 
 module.exports = client;
